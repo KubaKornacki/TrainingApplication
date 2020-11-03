890b8da8f54cbabea31aa0f2166623430fdf50db
Jakub Kornacki <s15570@pjwstk.edu.pl>
2020-11-03 20:41:17 +0100

    added pushpull training and splitTraining

--- App_v2/TrainingGenerator/PushPullTraining.cs
+++ App_v2/TrainingGenerator/PushPullTraining.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using App_v2.Models;
+using App_v2.Tools;
 
 namespace App_v2.TrainingGenerator
 {
@@ -12,7 +13,71 @@ namespace App_v2.TrainingGenerator
         {
             if (trainingParameters.trainingType == 2)
             {
-                return null;
+                List<TrainingExercise> excercises = new List<TrainingExercise>();
+                List<Excercise> tmp = new List<Excercise>();
+
+                //Trening A
+                Subtraining subtraining1 = new Subtraining();
+                subtraining1.Name = "Push";
+                subtraining1.Training = training;
+                //nogi
+                Excercise excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 3 && x.Priority == 1 && trainingParameters.trainingKind==x.Machine);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+                tmp.Add(excercises.Last().Excercise);
+                //klata
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 1 && x.Priority == 21);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+                tmp.Add(excercises.Last().Excercise);
+                //barki
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 7 && x.Priority == 61 && trainingParameters.trainingKind == x.Machine);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 10.0, subtraining1));
+                tmp.Add(excercises.Last().Excercise);
+                //triceps
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 5 && x.Priority == 83);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 6.0, subtraining1));
+                tmp.Add(excercises.Last().Excercise);
+                if(trainingParameters.trainingGoal==1|| trainingParameters.trainingGoal == 3)
+                {
+                    excercise = new Excercise();
+                    Excercise excerciseTmp = excercises.FirstOrDefault(x => x.Excercise.PrimaryMuscle == trainingParameters.trainingGoal).Excercise;
+                    //dodatkowe
+                    excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == trainingParameters.trainingGoal && tmp.FirstOrDefault(y => y.PrimaryMuscle == trainingParameters.trainingGoal).Priority < x.Priority);
+                    excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+                    tmp.Add(excercises.Last().Excercise);
+                }
+
+                //Trening B
+                Subtraining subtraining2 = new Subtraining();
+                subtraining2.Name = "Pull";
+                subtraining2.Training = training;
+                tmp = new List<Excercise>();
+                //nogi
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 3 && x.Priority == 5);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+                tmp.Add(excercises.Last().Excercise);
+                //plecy
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 2 && x.Priority == 41 && trainingParameters.trainingKind == x.Machine);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+                tmp.Add(excercises.Last().Excercise);
+                //biceps
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 4 && x.Priority == 103);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 6.0, subtraining2));
+                tmp.Add(excercises.Last().Excercise);
+                //brzuch
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 6 && x.Priority == 121 && trainingParameters.trainingKind == x.Machine);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+                tmp.Add(excercises.Last().Excercise);
+                if (trainingParameters.trainingGoal == 2)
+                {
+                    excercise = new Excercise();
+                    Excercise excerciseTmp = excercises.FirstOrDefault(x => x.Excercise.PrimaryMuscle == trainingParameters.trainingGoal).Excercise;
+                    //dodatkowe
+                    excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == trainingParameters.trainingGoal && tmp.FirstOrDefault(y => y.PrimaryMuscle == trainingParameters.trainingGoal).Priority < x.Priority);
+                    excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+                    tmp.Add(excercises.Last().Excercise);
+                }
+
+                return excercises;
             }
             else
             {
--- App_v2/TrainingGenerator/SplitTraining.cs
+++ App_v2/TrainingGenerator/SplitTraining.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using App_v2.Models;
+using App_v2.Tools;
 
 namespace App_v2.TrainingGenerator
 {
@@ -12,7 +13,104 @@ namespace App_v2.TrainingGenerator
         {
             if (trainingParameters.trainingType == 3)
             {
-                return null;
+                List<TrainingExercise> excercises = new List<TrainingExercise>();
+
+                //Trening A
+                Subtraining subtraining1 = new Subtraining();
+                subtraining1.Name = "Plecy +Biceps";
+                subtraining1.Training = training;
+                //plecy
+                Excercise excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 2 && x.Priority == 41);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+
+                //plecy
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 2 && x.Priority == 47 && x.Machine == trainingParameters.trainingKind);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+
+                //biceps
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 4 && x.Priority == 101);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+
+                //biceps
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 4 && x.Priority == 102);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+
+                if(trainingParameters.trainingGoal==3)
+                {
+                    excercise = new Excercise();
+                    Excercise excerciseTmp = excercises.FirstOrDefault(x => x.Excercise.PrimaryMuscle == trainingParameters.trainingGoal).Excercise;
+                    //dodatkowe
+                    excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 2 && x.Priority==46 && x.Machine == trainingParameters.trainingKind);
+                    excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining1));
+
+                }
+
+                //Trening B
+                Subtraining subtraining2 = new Subtraining();
+                subtraining2.Name = "Klata + Triceps";
+                subtraining2.Training = training;
+
+                //klata
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 1 && x.Priority == 21);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+
+                //klata
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 1 && x.Priority == 25 && x.Machine == trainingParameters.trainingKind);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+
+                //triceps
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 5 && x.Priority == 81);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+
+                //triceps
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 5 && x.Priority == 86);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+
+                if (trainingParameters.trainingGoal == 1)
+                {
+                    excercise = new Excercise();
+                    Excercise excerciseTmp = excercises.FirstOrDefault(x => x.Excercise.PrimaryMuscle == trainingParameters.trainingGoal).Excercise;
+                    //dodatkowe
+                    excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 1 && x.Priority == 23 && x.Machine == trainingParameters.trainingKind);
+                    excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining2));
+
+                }
+
+                //Trening C
+                Subtraining subtraining3 = new Subtraining();
+                subtraining3.Name = "Nogi + brzuch";
+                subtraining3.Training = training;
+
+                //nogi
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 3 && x.Priority == 1 && x.Machine == trainingParameters.trainingKind);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining3));
+
+                //nogi
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 3 && x.Priority == 4);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining3));
+
+                //brzuch
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 6 && x.Priority == 121 && x.Machine == trainingParameters.trainingKind);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining3));
+
+                //brzuch
+                excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 6 && x.Priority == 122 && x.Machine == trainingParameters.trainingKind);
+                excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 20.0, subtraining3));
+
+                if (trainingParameters.trainingGoal == 3)
+                {
+                    excercise = new Excercise();
+                    Excercise excerciseTmp = excercises.FirstOrDefault(x => x.Excercise.PrimaryMuscle == trainingParameters.trainingGoal).Excercise;
+                    //dodatkowe
+                    excercise = dbContext.Excercises.FirstOrDefault(x => x.PrimaryMuscle == 3 && x.Priority == 3);
+                    excercises.Add(GlobalFunctions.SetTrainingExercise(excercise, trainingParameters, 10.0, subtraining3));
+
+                }
+
+
+
+
+                return excercises;
             }
             else
             {
